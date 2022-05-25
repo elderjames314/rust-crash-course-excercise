899bad0a007c00909e7a186d1abc6c52d8550d48
James Oladimeji <elderjames314@gmail.com>
2022-05-25 07:46:38 +0100

    add adjustment to excercise A

--- work/excerciseA/src/main.rs
+++ work/excerciseA/src/main.rs
@@ -1,10 +1,8 @@
-const MISSILE:i32 = 8;
-const READY:i32 = 2;
 const STARTING_MISSILES: i32 = 8;
 const STARTING_AMOUNT: i32 = 2;
 fn main() {
-    let (mut missiles,  ready)  = (8, 2);
-    println!("Firing {} of my {} missiles...", READY, MISSILE);
+    let (mut missiles,  ready)  = (STARTING_MISSILES, STARTING_AMOUNT);
+    println!("Firing {} of my {} missiles...", ready, missiles);
     missiles = missiles - ready;
     println!("Firing {} of my {} missiles...", ready, missiles);
 
